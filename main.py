29bf70767915e455936d2ca5ce5ca50c3fd24071
Mikee <michaelwolfsonkin@gmail.com>
2022-04-25 16:43:07 -0400

    Implemented point_inside function using winding number with arctan

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from pygame import QUIT, KEYDOWN
 from pygame.locals import K_ESCAPE, K_RETURN
 
 from polygon_builder import PolygonBuilder
-from config import WINDOW_TITLE, WINDOW_SIZE
+from config import WINDOW_TITLE, WINDOW_SIZE, RED, BLUE
 
-from polygon_utils import is_ccw
+from polygon_utils import is_ccw, point_inside
 
 def main():
 	pygame.init()
@@ -24,6 +24,15 @@ def main():
 			polygon = poly_builder.get_polygon()
 			if polygon:
 				print(f'is_ccw {is_ccw(polygon)}')
+
+				for x in range(0,WINDOW_SIZE[0],5):
+					for y in range(0,WINDOW_SIZE[1],5):
+						p = (x,y)
+						color = BLUE if point_inside(polygon, p) else RED
+						pygame.draw.circle(screen, color, p, 1)
+				pygame.display.flip() # Update the display
+
+
 
 def is_quit_event(event):
 	return event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE
--- polygon_utils.py
+++ polygon_utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def is_ccw(poly, inverted_y_axis = True):
 	twice_area = 0
 	n = len(poly)
@@ -6,3 +8,28 @@ def is_ccw(poly, inverted_y_axis = True):
 		b = poly[(i+1)%n]
 		twice_area += (b[0]-a[0])*(b[1]+a[1])
 	return twice_area > 0 if inverted_y_axis else twice_area < 0
+
+def point_inside(poly, point, method='winding number'):
+	if method == 'winding number':
+		return not np.isclose(_winding_number(poly, point),0)
+	elif method == 'ray casting':
+		pass
+	else:
+		raise ValueError('Variable method should be "winding number" or "ray casting"')
+
+def _winding_number(poly, point):
+	poly = np.array(poly)
+	poly -= point # center point on origin
+	
+	angles = np.arctan2(poly[:,1],poly[:,0]) # [-pi, pi]
+	
+	deltas = np.copy(angles)
+	deltas[1:] -= angles[:-1]
+	deltas[0] -= angles[-1]
+	
+	deltas[deltas <= -np.pi] += 2*np.pi
+	deltas[deltas >= np.pi] -= 2*np.pi
+
+	winding_num = np.rint(np.sum(deltas) / (2*np.pi))
+
+	return winding_num
